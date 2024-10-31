51a30738d4d6b2abe6f3c2066e8b0d0132308f60
Mykyta Karpin <mkarpin@mirantis.com>
2024-10-31 07:37:18 +0000

    Fix attribute of falcon response
    Change-Id: I5bda25fbf4f17029d1badc3e09e47531ccd298e6
    Related-Prod: https://mirantis.jira.com/browse/PRODX-47710

--- openstack_refapp/root.py
+++ openstack_refapp/root.py
@@ -11,6 +11,6 @@ class RootResource:
     def on_get(self, req, resp):
         LOG.info(f"got request {req}")
         resp.status = falcon.HTTP_200
-        resp.body = json.dumps(
+        resp.text = json.dumps(
             dict(app="openstack-refapp", host=platform.node())
         )
